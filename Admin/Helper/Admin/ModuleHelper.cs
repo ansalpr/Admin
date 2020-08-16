ea926a248dd48c613d0fb8cd8cb51e57945fada0
ansalpr <67136006+ansalpr@users.noreply.github.com>
2020-08-16 22:29:54 +0530

    Insert entity Correction
    Insert entity Correction

--- Admin/Helper/Admin/ModuleHelper.cs
+++ Admin/Helper/Admin/ModuleHelper.cs
@@ -246,7 +246,6 @@ namespace Admin.Helper.Admin
                 entityObject.RecordStatus = 0;
                 result = DO.iteratePropertyObjectsAndInsert(entityObject, TableName, true, skipAttributes);
                 DO.EndTRansaction();
-                result = 1;
             }
             catch (Exception ex)
             {
